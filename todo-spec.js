6962605de4720022698d8950490ad10be2c5ed05
kanle05 <kanle05@ca.com>
2017-09-29 11:05:08 -0500

    Update todo-spec.js

--- todo-spec.js
+++ todo-spec.js
@@ -1,7 +1,7 @@
-describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
+describe('Modern Software Factory UI Test - CACDINDD Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://<preprod_url>');
+    browser.get('http://cacdddemo.cdbu.io');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
